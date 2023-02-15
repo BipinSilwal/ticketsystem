7665ec9fa22e646a26589b22de6c77df3c67dd3a
bipin <bpnsilwal12@gmail.com>
2023-02-15 15:22:39 +0545

    error-Handler

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,9 +1,30 @@
-export const registerUser = (req, res) => {
-  console.log(req.url);
-  res.json({ message: 'Register Users' });
-};
+//@ Register a new user
+// @route /api/users
+// @access public
 
-export const LoginUser = (req, res) => {
+import asyncHandler from 'express-async-handler';
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = await req.body;
+
+  if (!name || !email || !password) {
+    throw new Error('please include all fields', 401);
+  }
+
+  return res.status(200).json({
+    message: 'registered Successfully!!',
+    value: {
+      name,
+      email,
+      password,
+    },
+  });
+});
+
+//@ Login a new user
+// @route /api/login
+// @access public
+export const LoginUser = asyncHandler(async (req, res) => {
   console.log(req.url);
   res.json({ message: 'Login Users' });
-};
+});
