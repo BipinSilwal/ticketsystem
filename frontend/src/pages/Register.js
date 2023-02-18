8d9d549dd447bd3010a796414e21379bacc15eba
bipin <bpnsilwal12@gmail.com>
2023-02-18 16:50:33 +0545

    reduxtoolkit

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { register, reset } from '../features/auth/authSlice.js';
+
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,6 +16,24 @@ const Register = () => {
   });
 
   const { name, email, password, confirmPassword } = formData;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+    dispatch(reset());
+  }, [isError, isSuccess, user, navigate, message, dispatch]);
 
   const handleChange = (e) => {
     setFormData((prevState) => ({
@@ -26,9 +48,12 @@ const Register = () => {
     if (password !== confirmPassword) {
       toast.error('Passwords do not match!');
     } else {
-      toast.success('Registered Successfully!');
-      const data = formData;
-      console.log(JSON.stringify(data));
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
     }
   };
 
@@ -37,7 +62,7 @@ const Register = () => {
       <section className="heading">
         <h1>
           <FaUser />
-          Register
+          Register {user}
         </h1>
         <p>Please create an account</p>
       </section>
